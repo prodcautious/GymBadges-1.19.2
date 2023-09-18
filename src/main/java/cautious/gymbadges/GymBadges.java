c1d71186bff0011c55b896ab1b7c4f6c821e15d5
prodcautious <125836095+prodcautious@users.noreply.github.com>
2023-09-18 14:58:24 -0400

    Initial commit

---
+++ src/main/java/cautious/gymbadges/GymBadges.java
@@ -0,0 +1,17 @@
+package cautious.gymbadges;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GymBadges implements ModInitializer {
+	public static final String MOD_ID = "gymbadges";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/cautious/gymbadges/GymBadgesDataGenerator.java
@@ -0,0 +1,11 @@
+package cautious.gymbadges;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class GymBadgesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
